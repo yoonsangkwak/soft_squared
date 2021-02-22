2d1f376375607f406976769b28152e6f8a87227b
yoonsangkwak <yunsang08@naver.com>
2021-02-22 22:12:43 +0900

    add activity

---
+++ MyListView/app/src/main/java/site/yoonsang/mylistview/AddActivity.kt
@@ -0,0 +1,28 @@
+package site.yoonsang.mylistview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import site.yoonsang.mylistview.databinding.ActivityAddBinding
+
+class AddActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityAddBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityAddBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val peopleActiveList = arrayListOf<PeopleActive>()
+
+        for (i in 1..10) {
+            peopleActiveList.add(PeopleActive("김덕배", R.drawable.user))
+        }
+
+        binding.addRecyclerView.apply {
+            layoutManager = LinearLayoutManager(context)
+            adapter = PeopleActiveAdapter(context, peopleActiveList)
+        }
+    }
+}
